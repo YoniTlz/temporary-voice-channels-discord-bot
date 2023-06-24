6f296c004384aebad464c6fa2f21c566568a307e
Yoni THELISE <yoni.thelise@gmail.com>
2023-06-24 15:08:12 +0200

    fix: modify free game command

--- src/main/kotlin/space/astrobot/discord/slashcommands/epicgames/FreeGames.kt
+++ src/main/kotlin/space/astrobot/discord/slashcommands/epicgames/FreeGames.kt
@@ -1,25 +1,77 @@
 package space.astrobot.discord.slashcommands.epicgames
 
+import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.interactions.commands.Command
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import space.astrobot.discord.interactionsLogic.slashcommands.SlashCommand
 import space.astrobot.discord.interactionsLogic.slashcommands.SlashCommandCTX
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
-class FreeGames: SlashCommand(
-    name = "free-games",
-    //description = "Gets the list of weekly Epic Games Store free games",
-    description = "Récupère la liste des jeux de la semaine gratuits sur Epic Games Store",
-    parentSlashCommand = Epic(),
+class FreeGames : SlashCommand(
+    name = "jeux-gratuits",
+    description = "Commande qui récupère la liste des jeux gratuits par plateforme",
+    options = listOf(
+        OptionData(OptionType.STRING, "plateforme", "Le nom de la plateforme", true)
+            .addChoices(
+                Command.Choice("PS Plus", "ps-plus"),
+                Command.Choice("Epic Games", "epic-games")
+            )
+    )
 ) {
     override suspend fun execute(ctx: SlashCommandCTX) {
-        println("////ici")
-//        val client = HttpClient.newBuilder().build();
-//        val request = HttpRequest.newBuilder()
-//            .uri(URI.create("http://yonitlz.synology.me/epic-free-games?isDev=true"))
-//            .build();
-//        val response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//        println(response.body())
+        val action = ctx.getOption<String>(options[0].name)!!
+        var channelId = ctx.channel.id
+        var url = ""
+        var plateforme = ""
+
+        when (action) {
+            "ps-plus" -> {
+                url = "https://yonitlz.synology.me/ps-plus-monthly-games?channelId=$channelId"
+                plateforme = "PS Plus"
+            }
+
+            "epic-games" -> {
+                url = "https://yonitlz.synology.me/epic-free-games?channelId=$channelId"
+                plateforme = "Epic Games"
+            }
+        }
+
+        val (trustAllCerts, sslSocketFactory) = prepareCerts()
+        val client = OkHttpClient.Builder().sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }.build()
+        val request = Request.Builder().url(url)
+            .build()
+        client.newCall(request).execute()
+
+        ctx.reply("Récupération des jeux gratuits - **$plateforme**")
+    }
+
+    private fun prepareCerts(): Pair<Array<TrustManager>, SSLSocketFactory> {
+        //set self sign certificate
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+            }
+
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+            }
+
+            override fun getAcceptedIssuers() = arrayOf<X509Certificate>()
+        })
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+
+        // Create an ssl socket factory with our all-trusting manager
+        val sslSocketFactory = sslContext.socketFactory
+        return Pair(trustAllCerts, sslSocketFactory)
     }
 }
+
+
